9b83b34af1052f68bd1809313331b9d34da29c09
Nathan51888 <nathanliang99@outlook.com>
2024-03-13 22:02:30 +1300

    changed api http method

--- src/routes/api1Router.js
+++ src/routes/api1Router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const api1Controller = require("../controller/api1Controller");
 
-router.get("/", api1Controller.getCarValue);
+router.post("/", api1Controller.getCarValue);
 
 module.exports = router;
